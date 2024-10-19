0a2113cdef32479aba97c2c03a459dc57484efbf
Hristifor <158781086+Hristifor3001003@users.noreply.github.com>
2024-10-19 12:39:05 +0200

    Add files via upload

---
+++ C# && .NET/Count Colored Cells/The Solution/Count Total Number Of Colored Cells/Count Total Number Of Colored Cells/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Count_Total_Number_Of_Colored_Cells
+{
+    internal class Program
+    {
+
+        public static long ColoredCells(int n)
+        {
+
+            return (2 * (long)n * (n - 1) + 1);
+        }
+        static void Main(string[] args)
+        {
+            for(int n = 1; n <= 50; n++)
+            {
+                Console.WriteLine("For n = " + n + ", we have: " + ColoredCells(n) + " colored cells.");
+            }
+        }
+    }
+}
